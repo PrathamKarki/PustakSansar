156394393237b24c1b73895e8983319cfc98d0db
pratham karki <prathamkarki21@gmail.com>
2025-06-16 14:57:03 +0545

    fix cors issue

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -9,6 +9,8 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/com
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react';
+import axios from 'axios';
+
 
 interface RegisterFormValues {
   firstName: string;
@@ -56,8 +58,10 @@ const Register = () => {
 
   const handleSubmit = (values: RegisterFormValues, { setSubmitting }: any) => {
     
+    console.log('Registration form submitted:', values);
+    axios.post('http://localhost:8080/register', values)
+
     setTimeout(() => {
-      console.log('Registration form submitted:', values);
       alert('Account Created Successfully! Welcome! Your account has been created.');
       setSubmitting(false);
     }, 1000);
